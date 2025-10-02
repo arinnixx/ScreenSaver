777b96d47d5ce3d9c416885086f5636a58396ee9
arinnixx <tyrtyshnaya.a.0@mail.ru>
2025-10-02 20:10:20 +0300

    Итог

---
+++ ScreenSaver/ScreenSaver/Snowflake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSaver
+{
+    public class Snowflake
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Size { get; set; }
+        public int Speed { get; set; }
+    }
+}
